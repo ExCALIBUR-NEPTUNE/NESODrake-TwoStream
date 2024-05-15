d1d93970283e4343c108723c7fd8dfddecbbd734
Will Saunders <will.saunders@ukaea.uk>
2024-05-15 18:38:07 +0100

    passed DMPlex to two stream

--- src/two_stream.cpp
+++ src/two_stream.cpp
@@ -1,9 +1,55 @@
+#include <petsc.h>
+#include <petscfe.h>
+#include <petscdmtypes.h>
 #include <neso_particles.hpp>
 #include <pybind11/pybind11.h>
-namespace py = pybind11;
+#include <cstdint> 
+#include <cstring>
 
-inline int foo(){return 42;}
+namespace py = pybind11;
+using namespace NESO::Particles;
+
+struct TwoStreamParticles {
+  
+  int num_particles;
+  SYCLTargetSharedPtr sycl_target;
+  std::shared_ptr<PetscInterface::DMPlexInterface> mesh_interface;
+  DM dm;
+
+  TwoStreamParticles() = default;
+  TwoStreamParticles(
+    std::uintptr_t dm_vptr,
+    const int num_particles
+  ) : 
+    num_particles(num_particles),
+    sycl_target(std::make_shared<SYCLTarget>(0, PETSC_COMM_WORLD))
+  {
+    this->sycl_target->print_device_info();
+
+    int flag;
+    MPICHK(MPI_Initialized(&flag));
+    NESOASSERT(flag, "MPI is not initalised.");
+
+    this->dm = reinterpret_cast<DM>(dm_vptr);
+    PetscBool is_initialised;
+    PETSCCHK(PetscInitialized(&is_initialised));
+    NESOASSERT(is_initialised, "PETSc is not initialised");
+
+  }
+
+  void free(){
+    if (this->sycl_target){
+      this->sycl_target->free();
+    }
+  }
+};
+
 
 PYBIND11_MODULE(two_stream, m) {
-  m.def("foo", &foo);
+
+  py::class_<TwoStreamParticles>(m, "TwoStreamParticles")
+      .def(py::init<std::uintptr_t, const int>())
+      .def("free", &TwoStreamParticles::free);
+
+
 }
