433ba8206c6e49e279ae5b2e2bf1a7cfdf723765
Will Saunders <will.saunders@ukaea.uk>
2024-05-30 17:00:30 +0100

    two way coupling functioning

--- examples/prototype.py
+++ examples/prototype.py
@@ -10,48 +10,57 @@ def setup_project(particle_state, V):
     quadrature_points = X.dat.data_ro
     nrow, ncol = quadrature_points.shape
     ptr = X.dat.data_ro.ctypes._as_parameter_.value
-    particle_state.qpm_setup(nrow, ncol, ptr)
+    particle_state.setup_project(nrow, ncol, ptr)
 
-def project(particle_state, u, v = None):
+def setup_evaluate(particle_state, V):
+    m = V.mesh()
+    W = VectorFunctionSpace(m, V.ufl_element())
+    X = assemble(interpolate(m.coordinates, W))
+    quadrature_points = X.dat.data_ro
+    nrow, ncol = quadrature_points.shape
+    ptr = X.dat.data_ro.ctypes._as_parameter_.value
+    particle_state.setup_evaluate(nrow, ncol, ptr)
+
+def project(particle_state, v, u):
     space = np.zeros_like(u.dat.data)
     nrow = space.shape[0]
     ncol = 1
-    particle_state.qpm_project(nrow, ncol, space.ctypes._as_parameter_.value)
+    particle_state.project(nrow, ncol, space.ctypes._as_parameter_.value)
     u.dat.data[:] = space
-    if v is not None:
-        v.interpolate(u)
+    v.interpolate(u)
 
-def evaluate(particle_state, u, v = None):
-    if v is not None:
-        u.interpolate(v)
-    space = u.dat.data_ro
-    nrow = space.shape[0]
-    ncol = 1
-    particle_state.qpm_evaluate(nrow, ncol, space.ctypes._as_parameter_.value)
+def evaluate(particle_state, u, v):
+    for cx in range(2):
+        v.interpolate(u[cx])
+        space = v.dat.data_ro
+        nrow = space.shape[0]
+        ncol = 1
+        particle_state.evaluate(cx, nrow, ncol, space.ctypes._as_parameter_.value)
 
 
 if __name__ == "__main__":
     
-    num_steps = 200
+    num_steps = 20
+    num_print_steps = 1
     num_cells = 16
     num_particles = 100000
 
-    #mesh = PeriodicUnitSquareMesh(
+    mesh_np = PeriodicUnitSquareMesh(
+        num_cells, 
+        num_cells
+    )
     mesh = UnitSquareMesh(
         num_cells, 
         num_cells
     )
-    mesh_firedrake = UnitSquareMesh(
-        num_cells, 
-        num_cells
-    )
-    
-    V = FunctionSpace(mesh, "DG", 0)
-    V_firedrake = FunctionSpace(mesh_firedrake, "DG", 0)
-    
-    u = Function(V, name="u")
-    v = Function(V_firedrake)
 
+    BDM_np = FunctionSpace(mesh_np, "BDM", 1)
+    DG_np = FunctionSpace(mesh_np, "DG", 0)
+    interp_intermediate = Function(DG_np)
+
+    BDM = FunctionSpace(mesh, "BDM", 1)
+    DG = FunctionSpace(mesh, "DG", 0)
+    W = BDM * DG
 
     # Immediately pass the DM to NESO-Particles before Firedrake adds any halo
     # cells.
@@ -60,23 +69,39 @@ if __name__ == "__main__":
         num_particles,
         0.0005
     )
+    
+    net_charge_density = Constant(particle_state.get_net_charge_density())
+
     particle_state.validate_halos()
     particle_state.add_particles()
 
-    setup_project(particle_state, V)
-    project(particle_state, u)
-    evaluate(particle_state, u)
-
-    outfile = VTKFile("mesh.pvd")
-    outfile.write(mesh)
-
-    outfile = VTKFile("u.pvd")
-    outfile.write(u)
+    setup_project(particle_state, DG_np)
+    setup_evaluate(particle_state, DG_np)
+    #project(particle_state, u)
+    #evaluate(particle_state, u)
     
+    E = Function(BDM)
+    phi = Function(DG)
+
+    x, y = SpatialCoordinate(mesh)
+    E.interpolate(
+        as_vector([cos(4*y), -sin(5*x)]),
+    )
+    
+    project(particle_state, phi, interp_intermediate)
+    evaluate(particle_state, E, interp_intermediate)
+
+    out_phi = VTKFile("phi.pvd")
+    out_phi.write(phi)
+    out_E = VTKFile("E.pvd")
+    out_E.write(E) 
+
     particle_state.write();
 
+    particle_state.free(); quit()
+
     for stepx in range(num_steps):
-        if (stepx % 10 == 0):
+        if (stepx % num_print_steps == 0):
             if mpi.COMM_WORLD.rank == 0:
                 print(stepx)
 
--- src/two_stream.cpp
+++ src/two_stream.cpp
@@ -5,6 +5,7 @@
 #include <pybind11/pybind11.h>
 #include <cstdint> 
 #include <cstring>
+#include <cmath>
 
 namespace py = pybind11;
 using namespace NESO::Particles;
@@ -30,6 +31,7 @@ struct TwoStreamParticles {
   int num_particles;
   int ndim;
   double dt;
+  double net_charge_density;
   SYCLTargetSharedPtr sycl_target;
   std::shared_ptr<PetscInterface::DMPlexInterface> mesh;
   DomainSharedPtr domain;
@@ -39,10 +41,10 @@ struct TwoStreamParticles {
   ParticleLoopSharedPtr loop_advect;
   std::shared_ptr<H5Part> h5part;
 
-  std::unique_ptr<ExternalCommon::DOFMapperDG> dof_mapper_dg;
-  std::unique_ptr<ExternalCommon::QuadraturePointMapper> qpm;
-
-  std::shared_ptr<CellDatConst<REAL>> cdc_project;
+  std::shared_ptr<ExternalCommon::QuadraturePointMapper> qpm_project;
+  std::shared_ptr<ExternalCommon::QuadraturePointMapper> qpm_evaluate;
+  std::unique_ptr<PetscInterface::DMPlexProjectEvaluate> dpe_project;
+  std::unique_ptr<PetscInterface::DMPlexProjectEvaluate> dpe_evaluate;
 
   TwoStreamParticles() = default;
   TwoStreamParticles(
@@ -82,7 +84,9 @@ struct TwoStreamParticles {
                                ParticleProp(Sym<REAL>("M"), 1),
                                ParticleProp(Sym<REAL>("V"), 3),
                                ParticleProp(Sym<REAL>("B"), 3),
-                               ParticleProp(Sym<REAL>("E"), 3)};
+                               ParticleProp(Sym<REAL>("E0"), 1),
+                               ParticleProp(Sym<REAL>("E1"), 1)
+    };
 
     this->particle_group = std::make_shared<ParticleGroup>(
         this->domain, particle_spec, this->sycl_target);
@@ -106,7 +110,7 @@ struct TwoStreamParticles {
     const REAL k_dht = 0.5 * k_dt; 
     this->loop_advect = particle_loop(
       "ParticleSystem:boris", this->particle_group,
-      [=](auto B, auto E, auto Q, auto M, auto P, auto V) {
+      [=](auto B, auto E0, auto E1, auto Q, auto M, auto P, auto V) {
         const REAL QoM = Q.at(0) / M.at(0);
 
         const REAL scaling_t = QoM * k_dht;
@@ -126,9 +130,9 @@ struct TwoStreamParticles {
         const REAL V_2 = V.at(2);
 
         // The E dat contains d(phi)/dx not E -> multiply by -1.
-        const REAL v_minus_0 = V_0 + (-1.0 * E.at(0)) * scaling_t;
-        const REAL v_minus_1 = V_1 + (-1.0 * E.at(1)) * scaling_t;
-        const REAL v_minus_2 = V_2 + (-1.0 * E.at(2)) * scaling_t;
+        const REAL v_minus_0 = V_0 + (-1.0 * E0.at(0)) * scaling_t;
+        const REAL v_minus_1 = V_1 + (-1.0 * E1.at(0)) * scaling_t;
+        const REAL v_minus_2 = V_2;
 
         REAL v_prime_0, v_prime_1, v_prime_2;
         CROSS_PRODUCT_3D(v_minus_0, v_minus_1, v_minus_2, t_0, t_1,
@@ -147,9 +151,9 @@ struct TwoStreamParticles {
         v_plus_2 += v_minus_2;
 
         // The E dat contains d(phi)/dx not E -> multiply by -1.
-        V.at(0) = v_plus_0 + scaling_t * (-1.0 * E.at(0));
-        V.at(1) = v_plus_1 + scaling_t * (-1.0 * E.at(1));
-        V.at(2) = v_plus_2 + scaling_t * (-1.0 * E.at(2));
+        V.at(0) = v_plus_0 + scaling_t * (-1.0 * E0.at(0));
+        V.at(1) = v_plus_1 + scaling_t * (-1.0 * E1.at(0));
+        V.at(2) = v_plus_2;
 
         // update of position to next time step
         P.at(0) += k_dt * V.at(0);
@@ -157,17 +161,22 @@ struct TwoStreamParticles {
         // P.at(2) += k_dt * V.at(2);
       },
       Access::read(Sym<REAL>("B")), 
-      Access::read(Sym<REAL>("E")),
+      Access::read(Sym<REAL>("E0")),
+      Access::read(Sym<REAL>("E1")),
       Access::read(Sym<REAL>("Q")), 
       Access::read(Sym<REAL>("M")),
       Access::write(Sym<REAL>("P")),
       Access::write(Sym<REAL>("V"))
     );
 
-    this->qpm = std::make_unique<ExternalCommon::QuadraturePointMapper>(
+    this->qpm_project = std::make_shared<ExternalCommon::QuadraturePointMapper>(
       sycl_target, domain);
-    this->cdc_project = std::make_shared<CellDatConst<REAL>>(
-      this->sycl_target, mesh->get_cell_count(),1, 1);
+    this->qpm_evaluate = std::make_shared<ExternalCommon::QuadraturePointMapper>(
+      sycl_target, domain);
+    this->dpe_project = std::make_unique<PetscInterface::DMPlexProjectEvaluate>(
+      qpm_project, "DG", 0);
+    this->dpe_evaluate = std::make_unique<PetscInterface::DMPlexProjectEvaluate>(
+      qpm_evaluate, "DG", 0);
   }
 
   void write(){
@@ -177,7 +186,8 @@ struct TwoStreamParticles {
        this->particle_group,
        Sym<REAL>("P"),
        Sym<REAL>("V"), 
-       Sym<REAL>("E"), 
+       Sym<REAL>("E0"), 
+       Sym<REAL>("E1"), 
        Sym<REAL>("Q"), 
        Sym<INT>("CELL_ID"),
        Sym<INT>("NESO_MPI_RANK"),
@@ -205,21 +215,38 @@ struct TwoStreamParticles {
     long rstart, rend;
     const long size = this->sycl_target->comm_pair.size_parent;
     const long rank = this->sycl_target->comm_pair.rank_parent;
+    
+    const int cell_count_local = mesh->get_cell_count();
+    int cell_count_global;
+    MPICHK(MPI_Allreduce(&cell_count_local, &cell_count_global, 1,
+                  MPI_INT, MPI_SUM, this->sycl_target->comm_pair.comm_parent));
+    
+    const int npart_per_cell = std::ceil(((double) this->num_particles) / ((double) cell_count_global));
+    this->num_particles = cell_count_global * npart_per_cell;
+    const int N = npart_per_cell * cell_count_local;
 
-    get_decomp_1d(size, (long)this->num_particles, rank, &rstart, &rend);
-    const long N = rend - rstart;
+    const double volume = this->mesh->dmh->get_volume();
+
+
+    int global_id_start;
+    MPICHK(MPI_Exscan(&N, &global_id_start, 1,
+           MPI_INT, MPI_SUM, this->sycl_target->comm_pair.comm_parent));
     
     std::srand(std::time(nullptr));
     int seed = std::rand();
     std::mt19937 rng_phasespace(seed + rank);
     
-    double extents[2] = {1.0, 1.0};
-    auto positions = uniform_within_extents(
-      N, 2, extents, rng_phasespace);
+    std::vector<std::vector<double>> positions;
+    std::vector<int> cells;
+    PetscInterface::uniform_within_dmplex_cells(this->mesh, npart_per_cell, positions, cells);
 
     const REAL initial_velocity = 1.0;
-    const REAL particle_charge = -1.0 / this->num_particles;
-    const REAL particle_mass = 1.0 / this->num_particles;
+    const REAL charge_density = 105.27578027828649;
+    const REAL particle_number_density = 105.27578027828649;
+    const REAL number_physical_particles = particle_number_density * volume;
+    const REAL particle_charge =
+          charge_density * volume / number_physical_particles;
+    const REAL particle_mass = 1.0;
   
     ParticleSet initial_distribution(
       N, this->particle_group->get_particle_spec());
@@ -227,8 +254,7 @@ struct TwoStreamParticles {
     std::normal_distribution norm_dist{0.0, 1.0};
 
     for (int px = 0; px < N; px++) {
-
-      const bool species = px % 2;
+      const bool species = (global_id_start + px) % 2;
       // x position
       const double x0 = positions[0][px];
       initial_distribution[Sym<REAL>("P")][px][0] = x0;
@@ -256,8 +282,19 @@ struct TwoStreamParticles {
     }
 
     this->particle_group->add_particles_local(initial_distribution);
-    parallel_advection_initialisation(this->particle_group, 20);
     this->transfer_particles();
+    
+    auto ga_net_charge = std::make_shared<GlobalArray<REAL>>(this->sycl_target, 1);
+    ga_net_charge->fill(0.0);
+    particle_loop(
+      this->particle_group,
+      [=](auto Q, auto NET){
+        NET.add(0, Q.at(0));
+      },
+      Access::read(Sym<REAL>("Q")),
+      Access::add(ga_net_charge)
+    )->execute();
+    this->net_charge_density = ga_net_charge->get().at(0) / volume;
   }
   
   void transfer_particles() {
@@ -275,85 +312,60 @@ struct TwoStreamParticles {
     NESOASSERT(this->mesh->validate_halos(), "Halo validation failed.");
   }
 
+  double get_net_charge_density(){
+    return this->net_charge_density;
+  }
+
   void move(){
     this->loop_advect->execute();
     this->transfer_particles();
   }
 
-  void dof_mapper_create(
-    const int num_dofs
-  ){
-    this->dof_mapper_dg = std::make_unique<ExternalCommon::DOFMapperDG>(
-    sycl_target, this->mesh->dmh->get_cell_count(), num_dofs);
-  }
-
-  void dof_mapper_set(
-    const int cell,
-    const int dof,
-    const int index
-  ){
-    this->dof_mapper_dg->set(cell, dof, index);
-  }
-
-  void qpm_setup(
+  void setup_project(
     const int nrow,
     const int ncol,
     std::uintptr_t vptr
   ){
     NESOASSERT(ncol == this->ndim, "Quadrature points have a bad number of columns.");
     REAL * pts = reinterpret_cast<REAL *>(vptr);
-    this->qpm->add_points_initialise();
+    this->qpm_project->add_points_initialise();
     for (int rx = 0; rx<nrow; rx++) {
-      this->qpm->add_point(pts);
+      this->qpm_project->add_point(pts);
       pts += ndim;
     }
-    this->qpm->add_points_finalise();
+    this->qpm_project->add_points_finalise();
   }
 
-  void qpm_project(
+  void setup_evaluate(
+    const int nrow,
+    const int ncol,
+    std::uintptr_t vptr
+  ){
+    NESOASSERT(ncol == this->ndim, "Quadrature points have a bad number of columns.");
+    REAL * pts = reinterpret_cast<REAL *>(vptr);
+    this->qpm_evaluate->add_points_initialise();
+    for (int rx = 0; rx<nrow; rx++) {
+      this->qpm_evaluate->add_point(pts);
+      pts += ndim;
+    }
+    this->qpm_evaluate->add_points_finalise();
+  }
+
+  void project(
     const int nrow,
     const int ncol,
     std::uintptr_t vptr
     ){
 
-    this->cdc_project->fill(0);
-    particle_loop(
-      "TwoStreamParticles::project_0",
-      this->particle_group,
-      [=](
-        auto Q,
-        auto C
-      ){
-        C.fetch_add(0, 0, Q.at(0));
-      },
-      Access::read(Sym<REAL>("Q")),
-      Access::add(this->cdc_project)
-    )->execute();
-
-    //this->particle_group->print(Sym<REAL>("Q"));
-
-    particle_loop(
-      "TwoStreamParticles::project_1",
-      this->qpm->particle_group,
-      [=](
-        auto Q,
-        auto C
-      ){
-        Q.at(0) = C.at(0, 0);
-      },
-      Access::write(this->qpm->get_sym(1)),
-      Access::read(this->cdc_project)
-    )->execute();
-
-    //this->qpm->particle_group->print(this->qpm->get_sym(1));
-
+    this->dpe_project->project(this->particle_group, Sym<REAL>("Q"));
     std::vector<REAL> output;
-    this->qpm->get(1, output);
+    this->qpm_project->get(1, output);
     REAL * pts = reinterpret_cast<REAL *>(vptr);
     std::memcpy(pts, output.data(), nrow * ncol * sizeof(REAL));
   }
 
-  void qpm_evaluate(
+  void evaluate(
+    const int component,
     const int nrow,
     const int ncol,
     std::uintptr_t vptr
@@ -363,33 +375,11 @@ struct TwoStreamParticles {
     for(int cx=0 ; cx <(nrow*ncol) ; cx++){
       input.at(cx) = pts[cx];
     }
-    this->qpm->set(1, input);
-
-    particle_loop(
-      this->qpm->particle_group,
-      [=](auto Q, auto O, auto C) {
-        if (O.at(3) == 0) {
-          C.at(0, 0) = Q.at(0);
-        }
-      },
-      Access::read(qpm->get_sym(1)),
-      Access::read(Sym<INT>("ADDING_RANK_INDEX")),
-      Access::write(this->cdc_project))
-      ->execute();
-
-    particle_loop(
-      this->particle_group,
-      [=](auto E, auto C) {
-        E.at(0) = C.at(0, 0); 
-      },
-      Access::write(Sym<REAL>("E")),
-      Access::read(this->cdc_project)
-    )->execute();
+    this->qpm_evaluate->set(1, input);
+    this->dpe_evaluate->evaluate(
+      this->particle_group, Sym<REAL>("E" + std::to_string(component)));
   }
-
-
 };
-
 
 PYBIND11_MODULE(two_stream, m) {
 
@@ -400,10 +390,10 @@ PYBIND11_MODULE(two_stream, m) {
       .def("move", &TwoStreamParticles::move)
       .def("validate_halos", &TwoStreamParticles::validate_halos)
       .def("add_particles", &TwoStreamParticles::add_particles)
-      .def("dof_mapper_create", &TwoStreamParticles::dof_mapper_create)
-      .def("dof_mapper_set", &TwoStreamParticles::dof_mapper_set)
-      .def("qpm_setup", &TwoStreamParticles::qpm_setup)
-      .def("qpm_project", &TwoStreamParticles::qpm_project)
-      .def("qpm_evaluate", &TwoStreamParticles::qpm_evaluate);
+      .def("setup_project", &TwoStreamParticles::setup_project)
+      .def("setup_evaluate", &TwoStreamParticles::setup_evaluate)
+      .def("project", &TwoStreamParticles::project)
+      .def("evaluate", &TwoStreamParticles::evaluate)
+      .def("get_net_charge_density", &TwoStreamParticles::get_net_charge_density);
 
 }
